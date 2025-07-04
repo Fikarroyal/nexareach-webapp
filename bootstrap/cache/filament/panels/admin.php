4b44d40f752ca5b29b4dcc5ce959d7c0c91e4179
Fikar Royal <121270650+Fikarroyal@users.noreply.github.com>
2025-07-04 22:22:11 +0700

    Add files via upload

---
+++ bootstrap/cache/filament/panels/admin.php
@@ -0,0 +1,79 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'app.filament.resources.katalog-resource.pages.create-katalog' => 'App\\Filament\\Resources\\KatalogResource\\Pages\\CreateKatalog',
+    'app.filament.resources.katalog-resource.pages.edit-katalog' => 'App\\Filament\\Resources\\KatalogResource\\Pages\\EditKatalog',
+    'app.filament.resources.katalog-resource.pages.list-katalogs' => 'App\\Filament\\Resources\\KatalogResource\\Pages\\ListKatalogs',
+    'app.filament.resources.payment-resource.pages.create-payment' => 'App\\Filament\\Resources\\PaymentResource\\Pages\\CreatePayment',
+    'app.filament.resources.payment-resource.pages.edit-payment' => 'App\\Filament\\Resources\\PaymentResource\\Pages\\EditPayment',
+    'app.filament.resources.payment-resource.pages.list-payments' => 'App\\Filament\\Resources\\PaymentResource\\Pages\\ListPayments',
+    'app.filament.resources.transaksi-resource.pages.create-transaksi' => 'App\\Filament\\Resources\\TransaksiResource\\Pages\\CreateTransaksi',
+    'app.filament.resources.transaksi-resource.pages.edit-transaksi' => 'App\\Filament\\Resources\\TransaksiResource\\Pages\\EditTransaksi',
+    'app.filament.resources.transaksi-resource.pages.list-transaksis' => 'App\\Filament\\Resources\\TransaksiResource\\Pages\\ListTransaksis',
+    'app.filament.resources.transaksi-resource.widgets.transaksi-overview' => 'App\\Filament\\Resources\\TransaksiResource\\Widgets\\TransaksiOverview',
+    'app.filament.resources.user-resource.pages.create-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\CreateUser',
+    'app.filament.resources.user-resource.pages.edit-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\EditUser',
+    'app.filament.resources.user-resource.pages.list-users' => 'App\\Filament\\Resources\\UserResource\\Pages\\ListUsers',
+    'app.filament.resources.user-resource.pages.view-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\ViewUser',
+    'filament.pages.dashboard' => 'Filament\\Pages\\Dashboard',
+    'filament.widgets.account-widget' => 'Filament\\Widgets\\AccountWidget',
+    'filament.widgets.filament-info-widget' => 'Filament\\Widgets\\FilamentInfoWidget',
+    'filament.livewire.database-notifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'filament.pages.auth.edit-profile' => 'Filament\\Pages\\Auth\\EditProfile',
+    'filament.livewire.global-search' => 'Filament\\Livewire\\GlobalSearch',
+    'filament.livewire.notifications' => 'Filament\\Livewire\\Notifications',
+    'filament.pages.auth.login' => 'Filament\\Pages\\Auth\\Login',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    0 => 'Filament\\Pages\\Dashboard',
+  ),
+  'pageDirectories' => 
+  array (
+    0 => '/Users/mymac/Documents/nexareach-php-main/app/Filament/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Pages',
+  ),
+  'resources' => 
+  array (
+    '/Users/mymac/Documents/nexareach-php-main/app/Filament/Resources/KatalogResource.php' => 'App\\Filament\\Resources\\KatalogResource',
+    '/Users/mymac/Documents/nexareach-php-main/app/Filament/Resources/PaymentResource.php' => 'App\\Filament\\Resources\\PaymentResource',
+    '/Users/mymac/Documents/nexareach-php-main/app/Filament/Resources/TransaksiResource.php' => 'App\\Filament\\Resources\\TransaksiResource',
+    '/Users/mymac/Documents/nexareach-php-main/app/Filament/Resources/UserResource.php' => 'App\\Filament\\Resources\\UserResource',
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => '/Users/mymac/Documents/nexareach-php-main/app/Filament/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Resources',
+  ),
+  'widgets' => 
+  array (
+    0 => 'Filament\\Widgets\\AccountWidget',
+    1 => 'Filament\\Widgets\\FilamentInfoWidget',
+  ),
+  'widgetDirectories' => 
+  array (
+    0 => '/Users/mymac/Documents/nexareach-php-main/app/Filament/Widgets',
+  ),
+  'widgetNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Widgets',
+  ),
+);
